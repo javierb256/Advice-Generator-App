7855833fae3af07c8738ac4dffc7963e1980d447
= <44247277+javierb256@users.noreply.github.com>
2024-06-06 23:00:08 -0700

    added advice api call need to fix advice not updating

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,37 @@
 import dice from "../assets/icon-dice.svg";
+import { useState, useEffect} from "react";
 
 function Main(){
+   const [advice, setAdvice] = useState([]);
+   const [id, setId] = useState([]);
+   let count = 0;
+
+   
+   useEffect(() => {
+      makeApiCall();
+   }, [])
+   const makeApiCall = () => {
+      fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+         console.log(data.slip.advice)
+         setAdvice(data.slip.advice);
+         setId(data.slip.id)
+         count +1
+         console.log(count)
+      })
+      .catch((err) => {
+         console.log(err.message)
+      })
+   }
+
    return (
    <div className="advice-container">
-   <p>ADVICE #000</p>
-   <h1>Advice</h1>
+   <p>ADVICE #{id}</p>
+   <h1>"{advice}"</h1>
+   <p>{count}</p>
    <hr />
-   <button><img src={dice} /></button>
+   <button onClick={makeApiCall} ><img src={dice} /></button>
    </div>
    );
 }
